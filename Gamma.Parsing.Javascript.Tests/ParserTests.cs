b04435f613e8b7f23e7dc436f8590f0f232a19a3
Dereck <ak90730@ens.etsmtl.ca>
2023-12-12 10:14:06 -0500

    re-structure parser tests

--- Gamma.Parsing.Javascript.Tests/ParserTests.cs
+++ Gamma.Parsing.Javascript.Tests/ParserTests.cs
@@ -6,230 +6,239 @@ namespace Gamma.Parsing.Javascript.Tests;
 [TestFixture]
 public class ParserTests
 {
-    #region Assignments
-    [Test]
-    public void Parse_VariableAssignment_ReturnsBinaryExpressionNode()
+    [TestFixture]
+    public class Assignments
     {
-        var ast = RunTest<VariableDeclarationNode>("let x = 42;", "VariableDeclarationNode");
-        var declaration = ast.Declarations[0].As<BinaryExpressionNode>();
-        Assert.That("assignment", Is.EqualTo(declaration.Type));
-        Assert.That("identifier", Is.EqualTo(declaration.Left.Type));
-        Assert.That("number", Is.EqualTo(declaration.Right.Type));
-        Assert.That("x", Is.EqualTo(declaration.Left.As<IdentifierNode>().Name));
-        Assert.That(42, Is.EqualTo(declaration.Right.As<LiteralNode>().Value));
+        [Test]
+        public void Parse_VariableAssignment_ReturnsBinaryExpressionNode()
+        {
+            var ast = RunTest<VariableDeclarationNode>("let x = 42;", "VariableDeclarationNode");
+            var declaration = ast.Declarations[0].As<BinaryExpressionNode>();
+            Assert.That("assignment", Is.EqualTo(declaration.Type));
+            Assert.That("identifier", Is.EqualTo(declaration.Left.Type));
+            Assert.That("number", Is.EqualTo(declaration.Right.Type));
+            Assert.That("x", Is.EqualTo(declaration.Left.As<IdentifierNode>().Name));
+            Assert.That(42, Is.EqualTo(declaration.Right.As<LiteralNode>().Value));
+        }
+
+        [Test]
+        public void Parse_ExpressionWithVariable_ReturnsBinaryExpressionNode()
+        {
+            var ast = RunTest<VariableDeclarationNode>("let y = x + 10;", "VariableDeclarationNode");
+            var declaration = ast.Declarations[0].As<BinaryExpressionNode>();
+            Assert.That("assignment", Is.EqualTo(declaration.Type));
+            Assert.That("identifier", Is.EqualTo(declaration.Left.Type));
+            Assert.That("binary", Is.EqualTo(declaration.Right.Type));
+
+            Assert.That("y", Is.EqualTo(declaration.Left.As<IdentifierNode>().Name));
+            var right = declaration.Right.As<BinaryExpressionNode>();
+            Assert.That("identifier", Is.EqualTo(right.Left.Type));
+            Assert.That("+", Is.EqualTo(right.Operator));
+            Assert.That("number", Is.EqualTo(right.Right.Type));
+            var variable = right.Left.As<IdentifierNode>();
+            Assert.That("x", Is.EqualTo(variable.Name));
+            var literal = right.Right.As<LiteralNode>();
+            Assert.That(10, Is.EqualTo(literal.Value));
+        }
+
+        [Test]
+        public void Parse_ExpressionWithParentheses_ReturnsBinaryExpressionNode()
+        {
+            RunTest<VariableDeclarationNode>("let z = (y * 2) + x;", "VariableDeclarationNode");
+        }
+
+        [Test]
+        public void Parse_SimpleVariableAssignment_ReturnsBinaryExpressionNode()
+        {
+            RunTest("let a = 5;", "VariableDeclarationNode");
+        }
+
+        [Test]
+        public void Parse_ComplexExpression_ReturnsBinaryExpressionNode()
+        {
+            RunTest("let b = (a * 3) + 7;", "VariableDeclarationNode");
+        }
+
+        [Test]
+        public void Parse_ExpressionWithDivision_ReturnsBinaryExpressionNode()
+        {
+            RunTest("let c = b / (a + 2);", "VariableDeclarationNode");
+        }
+
+        [Test]
+        public void Parse_ExpressionWithSubtraction_ReturnsBinaryExpressionNode()
+        {
+            RunTest("let d = 2 * (c - b);", "VariableDeclarationNode");
+        }
+
+        [Test]
+        public void Parse_ExpressionWithMultiplication_ReturnsBinaryExpressionNode()
+        {
+            RunTest("let e = d / (c + 1) * 4;", "VariableDeclarationNode");
+        }
+
+        [Test]
+        public void Parse_ExpressionWithMultipleOperators_ReturnsBinaryExpressionNode()
+        {
+            RunTest("let f = (e + 3) / 2;", "VariableDeclarationNode");
+        }
+
+        [Test]
+        public void Parse_ExpressionWithComparisonOperator_ReturnsBinaryExpressionNode()
+        {
+            RunTest("let result = f >= 10;", "VariableDeclarationNode");
+        }
     }
 
-    [Test]
-    public void Parse_ExpressionWithVariable_ReturnsBinaryExpressionNode()
+    [TestFixture]
+    public class Increments 
     {
-        var ast = RunTest<VariableDeclarationNode>("let y = x + 10;", "VariableDeclarationNode");
-        var declaration = ast.Declarations[0].As<BinaryExpressionNode>();
-        Assert.That("assignment", Is.EqualTo(declaration.Type));
-        Assert.That("identifier", Is.EqualTo(declaration.Left.Type));
-        Assert.That("binary", Is.EqualTo(declaration.Right.Type));
+        [Test]
+        public void Parse_PostIncrement_ReturnsUnaryExpressionNode()
+        {
+            RunTest("x++;", "UnaryExpressionNode");
+        }
 
-        Assert.That("y", Is.EqualTo(declaration.Left.As<IdentifierNode>().Name));
-        var right = declaration.Right.As<BinaryExpressionNode>();
-        Assert.That("identifier", Is.EqualTo(right.Left.Type));
-        Assert.That("+", Is.EqualTo(right.Operator));
-        Assert.That("number", Is.EqualTo(right.Right.Type));
-        var variable = right.Left.As<IdentifierNode>();
-        Assert.That("x", Is.EqualTo(variable.Name));
-        var literal = right.Right.As<LiteralNode>();
-        Assert.That(10, Is.EqualTo(literal.Value));
+        [Test]
+        public void Parse_PreIncrement_ReturnsUnaryExpressionNode()
+        {
+            RunTest("++x;", "UnaryExpressionNode");
+        }
+
+        [Test]
+        public void Parse_PostDecrement_ReturnsUnaryExpressionNode()
+        {
+            RunTest("y--;", "UnaryExpressionNode");
+        }
+
+        [Test]
+        public void Parse_PreDecrement_ReturnsUnaryExpressionNode()
+        {
+            RunTest("--y;", "UnaryExpressionNode");
+        }
+
+        [Test]
+        public void Parse_CompoundAssignment_ReturnsBinaryExpressionNode()
+        {
+            RunTest("z += 10;", "BinaryExpressionNode");
+        }
+
+        [Test]
+        public void Parse_CompoundAssignmentWithVariable_ReturnsBinaryExpressionNode()
+        {
+            RunTest("a += b;", "BinaryExpressionNode");
+        }
     }
 
-    [Test]
-    public void Parse_ExpressionWithParentheses_ReturnsBinaryExpressionNode()
+    [TestFixture]
+    public class IfElseStatements
     {
-        RunTest<VariableDeclarationNode>("let z = (y * 2) + x;", "VariableDeclarationNode");
+        [Test]
+        public void Parse_IfStatementWithLiteralCondition_ReturnsIfStatementNode()
+        {
+            RunTest("if (true) { x = 42; }", "IfStatementNode");
+        }
+
+        [Test]
+        public void Parse_IfElseStatement_ReturnsIfStatementNode()
+        {
+            RunTest("if (x > 10) { x = 42; } else { x = 0; }", "IfStatementNode");
+        }
+
+        [Test]
+        public void Parse_NestedIfStatements_ReturnsIfStatementNode()
+        {
+            RunTest("if (x > 10) { if (y < 5) { z = 42; } }", "IfStatementNode");
+        }
+
+        [Test]
+        public void Parse_IfStatementWithoutBraces_ReturnsIfStatementNode()
+        {
+            RunTest("if (x > 10) x = 42;", "IfStatementNode");
+        }
+
+        [Test]
+        public void Parse_IfStatementWithBinaryExpressionCondition_ReturnsIfStatementNode()
+        {
+            RunTest("if (x + y >= 20) { z = 42; }", "IfStatementNode");
+        }
+
+        [Test]
+        public void Parse_IfElseIfElseStatement_ReturnsIfStatementNode()
+        {
+            RunTest("if (x > 10) { y = 5; } else if (x < 0) { y = -5; } else { y = 0; }", "IfStatementNode");
+        }
     }
 
-    [Test]
-    public void Parse_SimpleVariableAssignment_ReturnsBinaryExpressionNode()
+    [TestFixture]
+    public class ForLoops
     {
-        RunTest("let a = 5;", "VariableDeclarationNode");
+        [Test]
+        public void Parse_ForLoopWithLiteralCondition_ReturnsForStatementNode()
+        {
+            RunTest("for (let i = 0; i < 5; i++) { console.log(i); }", "ForStatementNode");
+        }
+
+        [Test]
+        public void Parse_ForLoopWithVariableCondition_ReturnsForStatementNode()
+        {
+            RunTest("for (let i = 0; i < x; i++) { console.log(i); }", "ForStatementNode");
+        }
+
+        [Test]
+        public void Parse_ForLoopWithMultipleStatements_ReturnsForStatementNode()
+        {
+            RunTest("for (let i = 0; i < 3; i++) { console.log(i); x = x + i; }", "ForStatementNode");
+        }
+
+        [Test]
+        public void Parse_ForLoopWithoutInitializer_ReturnsProgramtNode()
+        {
+            RunTest("let i = 0; for (; i < 3; i++) { console.log(i); }", "ProgramNode");
+        }
+
+        [Test]
+        public void Parse_ForLoopWithoutCondition_ReturnsProgramNode()
+        {
+            RunTest("let i = 0; for (; ; i++) { console.log(i); }", "ProgramNode");
+        }
+
+        [Test]
+        public void Parse_ForLoopWithoutIncrementor_ReturnsProgramNode()
+        {
+            RunTest("let i = 0; for (i; i < 3;) { console.log(i); }", "ProgramNode");
+        }
     }
 
-    [Test]
-    public void Parse_ComplexExpression_ReturnsBinaryExpressionNode()
+    [TestFixture]
+    public class FunctionDeclarations
     {
-        RunTest("let b = (a * 3) + 7;", "VariableDeclarationNode");
+        [Test]
+        public void TestSimpleFunctionDeclaration()
+        {
+            var code = "function simpleFunction() { var x; }";
+            var ast = RunTest(code, "FunctionDeclarationNode");
+
+            // Additional assertions based on your AST structure
+            Assert.That("simpleFunction", Is.EqualTo(((FunctionDeclarationNode)ast).Identifier.Name));
+            Assert.That(0, Is.EqualTo(((FunctionDeclarationNode)ast).Parameters.Count));
+            Assert.That("identifier", Is.EqualTo(ast.As<FunctionDeclarationNode>().Body.Type));
+        }
+
+        [Test]
+        public void TestFunctionDeclarationWithParameters()
+        {
+            var code = "function functionWithParams(param1, param2) { var x; }";
+            var ast = RunTest(code, "FunctionDeclarationNode");
+
+            // Additional assertions based on your AST structure
+            Assert.That("functionWithParams", Is.EqualTo(((FunctionDeclarationNode)ast).Identifier.Name));
+            Assert.That(2, Is.EqualTo(((FunctionDeclarationNode)ast).Parameters.Count));
+            Assert.That("param1", Is.EqualTo(((IdentifierNode)((FunctionDeclarationNode)ast).Parameters[0]).Name));
+            Assert.That("param2", Is.EqualTo(((IdentifierNode)((FunctionDeclarationNode)ast).Parameters[1]).Name));
+            Assert.That("identifier", Is.EqualTo(ast.As<FunctionDeclarationNode>().Body.Type));
+        }
     }
-
-    [Test]
-    public void Parse_ExpressionWithDivision_ReturnsBinaryExpressionNode()
-    {
-        RunTest("let c = b / (a + 2);", "VariableDeclarationNode");
-    }
-
-    [Test]
-    public void Parse_ExpressionWithSubtraction_ReturnsBinaryExpressionNode()
-    {
-        RunTest("let d = 2 * (c - b);", "VariableDeclarationNode");
-    }
-
-    [Test]
-    public void Parse_ExpressionWithMultiplication_ReturnsBinaryExpressionNode()
-    {
-        RunTest("let e = d / (c + 1) * 4;", "VariableDeclarationNode");
-    }
-
-    [Test]
-    public void Parse_ExpressionWithMultipleOperators_ReturnsBinaryExpressionNode()
-    {
-        RunTest("let f = (e + 3) / 2;", "VariableDeclarationNode");
-    }
-
-    [Test]
-    public void Parse_ExpressionWithComparisonOperator_ReturnsBinaryExpressionNode()
-    {
-        RunTest("let result = f >= 10;", "VariableDeclarationNode");
-    }
-
-    #endregion
-
-    #region Increments
-    [Test]
-    public void Parse_PostIncrement_ReturnsUnaryExpressionNode()
-    {
-        RunTest("x++;", "UnaryExpressionNode");
-    }
-
-    [Test]
-    public void Parse_PreIncrement_ReturnsUnaryExpressionNode()
-    {
-        RunTest("++x;", "UnaryExpressionNode");
-    }
-
-    [Test]
-    public void Parse_PostDecrement_ReturnsUnaryExpressionNode()
-    {
-        RunTest("y--;", "UnaryExpressionNode");
-    }
-
-    [Test]
-    public void Parse_PreDecrement_ReturnsUnaryExpressionNode()
-    {
-        RunTest("--y;", "UnaryExpressionNode");
-    }
-
-    [Test]
-    public void Parse_CompoundAssignment_ReturnsBinaryExpressionNode()
-    {
-        RunTest("z += 10;", "BinaryExpressionNode");
-    }
-
-    [Test]
-    public void Parse_CompoundAssignmentWithVariable_ReturnsBinaryExpressionNode()
-    {
-        RunTest("a += b;", "BinaryExpressionNode");
-    }
-    #endregion
-
-    #region IfElseStatements
-    [Test]
-    public void Parse_IfStatementWithLiteralCondition_ReturnsIfStatementNode()
-    {
-        RunTest("if (true) { x = 42; }", "IfStatementNode");
-    }
-
-    [Test]
-    public void Parse_IfElseStatement_ReturnsIfStatementNode()
-    {
-        RunTest("if (x > 10) { x = 42; } else { x = 0; }", "IfStatementNode");
-    }
-
-    [Test]
-    public void Parse_NestedIfStatements_ReturnsIfStatementNode()
-    {
-        RunTest("if (x > 10) { if (y < 5) { z = 42; } }", "IfStatementNode");
-    }
-
-    [Test]
-    public void Parse_IfStatementWithoutBraces_ReturnsIfStatementNode()
-    {
-        RunTest("if (x > 10) x = 42;", "IfStatementNode");
-    }
-
-    [Test]
-    public void Parse_IfStatementWithBinaryExpressionCondition_ReturnsIfStatementNode()
-    {
-        RunTest("if (x + y >= 20) { z = 42; }", "IfStatementNode");
-    }
-
-    [Test]
-    public void Parse_IfElseIfElseStatement_ReturnsIfStatementNode()
-    {
-        RunTest("if (x > 10) { y = 5; } else if (x < 0) { y = -5; } else { y = 0; }", "IfStatementNode");
-    }
-    #endregion
-
-    #region ForLoops
-    [Test]
-    public void Parse_ForLoopWithLiteralCondition_ReturnsForStatementNode()
-    {
-        RunTest("for (let i = 0; i < 5; i++) { console.log(i); }", "ForStatementNode");
-    }
-
-    [Test]
-    public void Parse_ForLoopWithVariableCondition_ReturnsForStatementNode()
-    {
-        RunTest("for (let i = 0; i < x; i++) { console.log(i); }", "ForStatementNode");
-    }
-
-    [Test]
-    public void Parse_ForLoopWithMultipleStatements_ReturnsForStatementNode()
-    {
-        RunTest("for (let i = 0; i < 3; i++) { console.log(i); x = x + i; }", "ForStatementNode");
-    }
-
-    [Test]
-    public void Parse_ForLoopWithoutInitializer_ReturnsProgramtNode()
-    {
-        RunTest("let i = 0; for (; i < 3; i++) { console.log(i); }", "ProgramNode");
-    }
-
-    [Test]
-    public void Parse_ForLoopWithoutCondition_ReturnsProgramNode()
-    {
-        RunTest("let i = 0; for (; ; i++) { console.log(i); }", "ProgramNode");
-    }
-
-    [Test]
-    public void Parse_ForLoopWithoutIncrementor_ReturnsProgramNode()
-    {
-        RunTest("let i = 0; for (i; i < 3;) { console.log(i); }", "ProgramNode");
-    }
-    #endregion
-
-    #region FunctionDeclarations
-    [Test]
-    public void TestSimpleFunctionDeclaration()
-    {
-        var code = "function simpleFunction() { var x; }";
-        var ast = RunTest(code, "FunctionDeclarationNode");
-
-        // Additional assertions based on your AST structure
-        Assert.That("simpleFunction", Is.EqualTo(((FunctionDeclarationNode)ast).Identifier.Name));
-        Assert.That(0, Is.EqualTo(((FunctionDeclarationNode)ast).Parameters.Count));
-        Assert.That("identifier", Is.EqualTo(ast.As<FunctionDeclarationNode>().Body.Type));
-    }
-
-    [Test]
-    public void TestFunctionDeclarationWithParameters()
-    {
-        var code = "function functionWithParams(param1, param2) { var x; }";
-        var ast = RunTest(code, "FunctionDeclarationNode");
-
-        // Additional assertions based on your AST structure
-        Assert.That("functionWithParams", Is.EqualTo(((FunctionDeclarationNode)ast).Identifier.Name));
-        Assert.That(2, Is.EqualTo(((FunctionDeclarationNode)ast).Parameters.Count));
-        Assert.That("param1", Is.EqualTo(((IdentifierNode)((FunctionDeclarationNode)ast).Parameters[0]).Name));
-        Assert.That("param2", Is.EqualTo(((IdentifierNode)((FunctionDeclarationNode)ast).Parameters[1]).Name));
-        Assert.That("identifier", Is.EqualTo(ast.As<FunctionDeclarationNode>().Body.Type));
-    }
-    #endregion
 
     private static AstNode RunTest(string code, string expectedNodeType)
     {
